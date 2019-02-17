8da12ce11f359a62d441118417f4efa02ca3c6ff
Bruno Eduardo DAngelo de Oliveira <bruno.eduardo.do@gmail.com>
2019-02-17 14:47:39 -0300

    Initial commit

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import { Router, Scene, Tabs } from 'react-native-router-flux';
+
+class AppNavigator extends Component {
+    static propTypes = {
+    
+    }
+
+    render() {
+        return (
+            null
+        );
+    };
+}
+
+export default AppNavigator;
