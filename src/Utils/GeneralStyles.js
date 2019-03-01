8224d70f5f808c45b810b2e7726592b45816b79f
Bruno Eduardo DAngelo de Oliveira <bruno.eduardo.do@gmail.com>
2019-03-01 01:10:27 -0300

    Added project general styles utility

---
+++ src/Utils/GeneralStyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const Style = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    containerRow: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    centerContentHorVert: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default Style;
---
+++ src/Utils/index.js
@@ -0,0 +1,3 @@
+import GeneralStyles from './GeneralStyles'
+
+export { GeneralStyles };
